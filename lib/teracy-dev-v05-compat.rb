4f39d736bacee3f15b2a0c007238294fb54c5648
Ly Minh Phuong <ly.minh.phuong.1403@gmail.com>
2018-10-10 06:32:33 +0700

    should port v0.5 features to this extension #1

--- lib/teracy-dev-v05-compat.rb
+++ lib/teracy-dev-v05-compat.rb
@@ -1,10 +1,10 @@
 require 'teracy-dev'
-
+require_relative 'processors/extension_path'
 
 module TeracyDevV05Compat
 
   def self.init
-
+    TeracyDev.register_processor(TeracyDevV05Compat::Processors::ExtensionPath.new)
   end
 
 end
